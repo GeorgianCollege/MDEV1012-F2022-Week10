1dabeb9b720735fdcd9969155df355da06168ed6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-16 08:57:22 -0500

    confirmed read/write with firebase

--- app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/MainActivity.kt
@@ -2,6 +2,10 @@ package ca.georgiancollege.mdev1012_f2022_week10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -14,8 +18,25 @@ class MainActivity : AppCompatActivity()
 
         // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("message")
 
+
+        val myRef = database.getReference("message")
         myRef.setValue("Hello, World!")
+
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue()
+                Log.i("firebase-value", "Value is: ${value.toString()}")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("firebase-error", "Failed to read value.", error.toException())
+            }
+        })
     }
 }
