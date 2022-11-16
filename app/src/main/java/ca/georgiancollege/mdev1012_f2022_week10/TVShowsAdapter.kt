a210e903483a8600670565c584d188814ba31739
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-16 10:56:32 -0500

    added TVShows Adapter

--- app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/TVShowsAdapter.kt
+++ app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/TVShowsAdapter.kt
@@ -1,6 +1,8 @@
 package ca.georgiancollege.mdev1012_f2022_week10
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,7 +14,23 @@ class TVShowsAdapter(private val dataSet: MutableList<TVShow>): RecyclerView.Ada
         val studio: TextView
 
         init{
-
+            title = view.findViewById(R.id.tv_show_title)
+            studio = view.findViewById(R.id.studio)
         }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.text_row_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.title.text = dataSet[position].title // index of the list
+        viewHolder.studio.text = dataSet[position].studio
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
     }
 }
