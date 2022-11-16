9b461c08cf21ee7ade4ac1fbe29134c2722e89cc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-16 08:34:26 -0500

    added dependencies for firebase

--- app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/MainActivity.kt
@@ -3,8 +3,10 @@ package ca.georgiancollege.mdev1012_f2022_week10
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
