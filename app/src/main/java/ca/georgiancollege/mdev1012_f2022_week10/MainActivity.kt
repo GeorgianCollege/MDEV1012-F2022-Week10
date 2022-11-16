8c7504bdbab223670056b4815cf56c6e482b53d7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-16 08:29:18 -0500

    initial commit

---
+++ app/src/main/java/ca/georgiancollege/mdev1012_f2022_week10/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.georgiancollege.mdev1012_f2022_week10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
